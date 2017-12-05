a457b1a4e5731371844d252b19672d0ff65b514e
a <502681863@qq.com>
2017-12-05 09:07:49 +0800

    改回去了

--- src/main/java/com/stefan/sell/sell/dto/OrderDTO.java
+++ src/main/java/com/stefan/sell/sell/dto/OrderDTO.java
@@ -1,4 +1,6 @@
 package com.stefan.sell.sell.dto;
+import com.stefan.sell.sell.enums.OrderStatusEnum;
+import com.stefan.sell.sell.enums.PayStatusEnum;
 import com.stefan.sell.sell.pojo.OrderDetail;
 import lombok.Data;
 
@@ -12,16 +14,16 @@ import java.util.List;
  */
 @Data
 public class OrderDTO {
+    /** 订单id. */
     private String orderId;
     private String buyerName;//买家名字
     private String buyerPhone;//买家电话
     private String buyerAddress;//买家地址
     private String buyerOpenid;//买家微信openid
     private BigDecimal orderAmount;//订单总金额
-    private Integer orderStatus;//订单状态, 默认为新下单
-    private Integer payStatus;//支付状态, 默认未支付
+    private Integer orderStatus= OrderStatusEnum.NEW.getCode();//订单状态, 默认为新下单
+    private Integer payStatus= PayStatusEnum.WAIT.getCode();//支付状态, 默认未支付
     private Date createTime;
     private Date updateTime;
-
     private List<OrderDetail> orderDetails;
 }
